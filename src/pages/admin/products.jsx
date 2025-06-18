cdcca9512945c5586162d1a635acba38d86e2e6d
Erick Mongare <erickmongaremose@gmail.com>
2025-06-18 16:09:05 +0300

    feat: display products from the backend

--- src/pages/admin/products.jsx
+++ src/pages/admin/products.jsx
@@ -1,10 +1,37 @@
 import { Plus } from 'lucide-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router';
+import SectionHeading from '../../components/section-heading';
+import ProductCard from '../../components/product-card';
 
 export default function Products() {
+	const [products, setProducts] = useState([]);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		fetch('http://localhost:8000/products')
+			.then((res) => res.json())
+			.then((data) => {
+				if (!Array.isArray(data)) {
+					throw {
+						message: 'Expected an array but got something else',
+						received: data,
+					};
+				}
+				setProducts(data);
+			})
+			.catch((error) => setError(error));
+	}, []);
+
+	console.log(error);
+	console.log(products);
+
 	return (
-		<div>
+		<div className="flex flex-col gap-5 ">
+			<SectionHeading
+				title={'Products'}
+				description={'View your product catalogue'}
+			/>
 			<Link to={'/dashboard/products/add'}>
 				<div className="h-40 w-60 rounded-xl bg-amber-800 p-10">
 					<Plus size={30} className="text-amber-500" />
@@ -14,6 +41,21 @@ export default function Products() {
 					</h2>
 				</div>
 			</Link>
+
+			<div className="grid max-md:grid-cols-2 grid-cols-4 gap-5 mt-10">
+				{products.map((product) => (
+					<ProductCard
+						key={product.id}
+						id={product.id}
+						title={product.name}
+						description={product.description}
+						image={product.image}
+						rating={product.rating}
+						category={product.category}
+						price={product.price}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
